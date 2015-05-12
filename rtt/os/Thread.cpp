MagnaboscoL/rtt

commit 945a341176982b81d07c216f748676f2b3f3ad2b

engine/activity: fixed blocking calls to ExecutionEngine::process()
Broadcasting condition variables without holding the associated mutex can block for some reasons.
This patch adds MutexLocks whereever the msg_cond is triggered.

From http://pubs.opengroup.org/onlinepubs/009695399/functions/pthread_cond_broadcast.html:
> The pthread_cond_broadcast() or pthread_cond_signal() functions may be called by a thread whether
> or not it currently owns the mutex that threads calling pthread_cond_wait() or pthread_cond_timedwait()
> have associated with the condition variable during their waits; however, if predictable scheduling
> behavior is required, then that mutex shall be locked by the thread calling pthread_cond_broadcast() or
> pthread_cond_signal().

This patch also clears the Thread active flag in Thread::terminate() is called and forces the thread
function to be terminated in the destructor of class Activity. This solves some issues with pure virtual
method calls during the destruction phase if threads are still running and triggering each other.

Signed-off-by: Johannes Meyer <johannes@intermodalics.eu>

rtt/os/Thread.cpp
@@ -626,6 +626,7 @@ namespace RTT {
             rtos_sem_signal(&sem);
 
             rtos_task_delete(&rtos_task); // this must join the thread.
+            active = false;
         }
 
         const char* Thread::getName() const

rtt/Activity.cpp
@@ -98,6 +98,10 @@ namespace RTT
     Activity::~Activity()
     {
         stop();
+
+        // We need to join the activity's thread before destruction as the thread function might still
+        // access member variables. Activity::stop() does not guarantuee to stop the underlying thread.
+        terminate();
     }
 
     os::ThreadInterface* Activity::thread() {
@@ -137,7 +141,10 @@ namespace RTT
         if ( ! Thread::isActive() )
             return false;
         //a trigger is always allowed when active
-        msg_cond.broadcast();
+        {
+            os::MutexLock lock(msg_lock);
+            msg_cond.broadcast();
+        }
         Thread::start();
         return true;
     }
@@ -153,7 +160,6 @@ namespace RTT
             return false;
         }
         mtimeout = true;
-        msg_cond.broadcast();
         Thread::start();
         return true;
     }

rtt/ExecutionEngine.cpp
@@ -124,6 +124,7 @@ namespace RTT
             assert(foo);
             if ( foo->execute() == false ){
                 foo->unloaded();
+                os::MutexLock lock(msg_lock);
                 msg_cond.broadcast(); // required for waitForFunctions() (3rd party thread)
             } else {
                 f_queue->enqueue( foo );
@@ -226,16 +227,16 @@ namespace RTT
                 assert( com );
                 com->executeAndDispose();
             }
-            // there's no need to hold the lock during
-            // emptying the queue. But we must hold the
-            // lock once between excuteAndDispose and the
-            // broadcast to avoid the race condition in
-            // waitForMessages().
-            // This allows us to recurse into processMessages.
-            MutexLock locker( msg_lock );
         }
-        if ( com )
-            msg_cond.broadcast(); // required for waitForMessages() (3rd party thread)
+
+        // there's no need to hold the lock during
+        // emptying the queue. But we must hold the
+        // lock once between excuteAndDispose and the
+        // broadcast to avoid the race condition in
+        // waitForMessages().
+        // This allows us to recurse into processMessages.
+        MutexLock locker( msg_lock );
+        msg_cond.broadcast(); // required for waitForMessages() (3rd party thread)
     }
 
     bool ExecutionEngine::process( DisposableInterface* c )
@@ -252,7 +253,10 @@ namespace RTT
 
             bool result = mqueue->enqueue( c );
             this->getActivity()->trigger();
-            msg_cond.broadcast(); // required for waitAndProcessMessages() (EE thread)
+            {
+                os::MutexLock lock(msg_lock);
+                msg_cond.broadcast(); // required for waitAndProcessMessages() (EE thread)
+            }
             return result;
         }
         return false;

commit 35f818f07490ed926ef4167012e06e5b77b56768

thread: fixed invalid read of task name after it already has been freed in ~Activity()
This is a regression from 945a341176982b81d07c216f748676f2b3f3ad2b.

Signed-off-by: Johannes Meyer <johannes@intermodalics.eu>

rtt/os/Thread.cpp
@@ -327,9 +327,7 @@ namespace RTT {
             if (this->isRunning())
                 this->stop();
 
-            log(Debug) << "Terminating " << this->getName() << endlog();
             terminate();
-            log(Debug) << " done" << endlog();
             rtos_sem_destroy(&sem);
 
         }
@@ -622,16 +620,22 @@ namespace RTT {
             // avoid callling twice.
             if (prepareForExit) return;
 
+            Logger::In in("Thread::terminate");
+            log(Debug) << "Terminating " << this->getName() << endlog();
+
             prepareForExit = true;
             rtos_sem_signal(&sem);
 
             rtos_task_delete(&rtos_task); // this must join the thread.
             active = false;
+            log(Debug) << " done" << endlog();
         }
 
         const char* Thread::getName() const
         {
-            return rtos_task_get_name(&rtos_task);
+            const char *name = rtos_task_get_name(&rtos_task);
+            if (!name) return "(deleted)";
+            return name;
         }
 
         void Thread::setMaxOverrun( int m )

rtt/os/ecos/fosi_internal.cpp
@@ -50,6 +50,7 @@ namespace RTT
 	INTERNAL_QUAL int rtos_task_delete_main(RTOS_TASK* main_task)
 	{
         free(main_task->name);
+        main_task->name = NULL;
 	    return 0;
 	}
 
@@ -177,6 +178,8 @@ namespace RTT
     INTERNAL_QUAL void rtos_task_delete(RTOS_TASK* mytask) {
       // Free name
       free(mytask->name);
+      mytask->name = NULL;
+
       // KG: Peter does not check return values, it appears...
       bool succeed = cyg_thread_delete(mytask->handle);
       if (succeed == false)

rtt/os/gnulinux/fosi_internal.cpp
@@ -79,6 +79,7 @@ namespace RTT
 	{
         pthread_attr_destroy( &(main_task->attr) );
         free(main_task->name);
+        main_task->name = NULL;
 	    return 0;
 	}
 
@@ -277,7 +278,8 @@ namespace RTT
 	INTERNAL_QUAL void rtos_task_delete(RTOS_TASK* mytask) {
         pthread_join( mytask->thread, 0);
         pthread_attr_destroy( &(mytask->attr) );
-	    free(mytask->name);
+        free(mytask->name);
+        mytask->name = NULL;
 	}
 
     INTERNAL_QUAL int rtos_task_check_scheduler(int* scheduler)

rtt/os/lxrt/fosi_internal.cpp
@@ -137,6 +137,7 @@ namespace RTT
             //stop_rt_timer();
             rt_task_delete(main_task->rtaitask);
             free(main_task->name);
+            main_task->name = NULL;
             munlockall();
             return 0;
         }
@@ -340,6 +341,7 @@ namespace RTT
                 Logger::log() << Logger::Critical << "Failed to join "<< mytask->name <<"."<< Logger::endl;
 
             free( mytask->name );
+            mytask->name = NULL;
             // rt_task_delete is done in wrapper !
         }
 

rtt/os/macosx/fosi_internal.cpp
@@ -63,6 +63,7 @@ namespace RTT
 	{
         pthread_attr_destroy( &(main_task->attr) );
         free( main_task->name );
+        main_task->name = NULL;
 	    return 0;
 	}
 
@@ -201,9 +202,10 @@ namespace RTT
 
 	INTERNAL_QUAL void rtos_task_delete(RTOS_TASK* mytask)
 	{
-            pthread_join( mytask->thread, 0);
-            pthread_attr_destroy( &(mytask->attr) );
-	    free(mytask->name);
+        pthread_join( mytask->thread, 0);
+        pthread_attr_destroy( &(mytask->attr) );
+        free(mytask->name);
+        mytask->name = NULL;
 	}
 
         INTERNAL_QUAL int rtos_task_check_scheduler(int* scheduler)

rtt/os/win32/fosi_internal.cpp
@@ -130,6 +130,7 @@ void ErrorHandler(LPTSTR lpszFunction)
         //pthread_attr_destroy( &(main_task->attr) );
 		// printf("rtos_task_delete_main");
 		free(main_task->name);
+        main_task->name = NULL;
 	    return 0;
 	}
 

rtt/os/xenomai/fosi_internal.cpp
@@ -172,6 +172,7 @@ namespace RTT
         {
             //rt_task_delete( &(main_task->xenotask) );
             free (main_task->name);
+            main_task->name = NULL;
             munlockall();
             return 0;
         }
@@ -450,6 +451,8 @@ namespace RTT
                 log(Error) << "Failed to join with thread " << mytask->name << endlog();
             }
             rt_task_delete(&(mytask->xenotask));
+            free(mytask->name);
+            mytask->name = NULL;
         }
 
         INTERNAL_QUAL int rtos_task_set_priority(RTOS_TASK * mytask, int priority)